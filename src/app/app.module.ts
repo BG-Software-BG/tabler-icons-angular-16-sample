efba9c23dc3d64d74aad370181564cf34775f0b0
Bartłomiej Gawęda <bgaweda@abis.krakow.pl>
2024-04-13 10:03:03 +0200

    Remove code commented out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, TablerIconModule.pick({ IconBrandAngular, IconABOff }), TablerIconComponent],
-  // imports: [BrowserModule, TablerIconModule.pick(icons), TablerIconComponent],
+  // imports: [BrowserModule, TablerIconModule.pick(icons), TablerIconComponent], // import all icons
   bootstrap: [AppComponent],
   providers: [
     { provide: TablerIconConfig, useValue: { color: '#898898', stroke: 2, size: 60 } }
